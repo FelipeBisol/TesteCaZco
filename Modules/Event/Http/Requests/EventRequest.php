0927cd4b1b52d81ca91bd6a86970e25654c42871
Felipe.Bisol <felipelipe636@gmail.com>
2022-07-06 20:52:40 -0300

    Create EventRequest

---
+++ Modules/Event/Http/Requests/EventRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Event\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventRequest extends FormRequest
+{
+
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'event_time' => ['required', 'date_format:'],
+            'email_to_notification' => ['required', 'email']
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O campo name é obrigatório',
+            'description.required' => 'O campo description é obrigatório',
+            'event_time' => 'O campo event_time é obrigatório',
+            'email_to_notification' => 'O campo email_to_notification é obrigatório'
+        ];
+    }
+}
