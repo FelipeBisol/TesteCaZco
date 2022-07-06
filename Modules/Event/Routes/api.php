cc53d1e6dfef989bdb7e9205ddf39ce227af131a
Felipe.Bisol <felipelipe636@gmail.com>
2022-07-06 20:52:14 -0300

    Create API routes

--- Modules/Event/Routes/api.php
+++ Modules/Event/Routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Modules\Event\Http\Controllers\EventController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/event', function (Request $request) {
-    return $request->user();
-});
+Route::get('events', [EventController::class, 'filters']);
+Route::post('event', [EventController::class, 'store']);
+Route::put('event/{id}', [EventController::class, 'update']);
+Route::delete('event/{id}', [EventController::class, 'destroy']);
